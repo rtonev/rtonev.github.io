719e46bef08076c7dc7d89976e87c4b6be0a04ee
Raden Tonev <rtonev@umich.edu>
2018-02-15 15:47:42 -0500

    adding bulk of code

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+(function($) {
+    "use strict";
+
+    window.sr = ScrollReveal();
+    sr.reveal('.animated-appearance', {distance:"0px", scale:0.99});
+
+
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    });
+
+})(jQuery);
