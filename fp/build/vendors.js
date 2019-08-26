ac919b757ec6d1e4486ab50896f1233ca8e921d6
Raden Tonev <rado.toneff@gmail.com>
2019-08-26 09:12:40 -0500

    small update

--- fp/build/vendors.js
+++ fp/build/vendors.js
@@ -73,6 +73,31 @@ exports.DeviceSkin = DeviceSkin;
 
 /***/ }),
 
+/***/ "../../../../Applications/Framer X.app/Contents/Resources/Server/lib/framer-package-loader.js!./node_modules/@framer/framer.device-skin-google-pixel-2-just-black/build/index.js":
+/*!****************************************************************************************************************************************************************************************!*\
+  !*** /Users/tonev/Applications/Framer X.app/Contents/Resources/Server/lib/framer-package-loader.js!./node_modules/@framer/framer.device-skin-google-pixel-2-just-black/build/index.js ***!
+  \****************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 1210;
+        this.imageHeight = 2513;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
+/***/ }),
+
 /***/ "../../../../Applications/Framer X.app/Contents/Resources/Server/lib/framer-package-loader.js!./node_modules/@framer/google.material-design/build/index.js":
 /*!******************************************************************************************************************************************************************!*\
   !*** /Users/tonev/Applications/Framer X.app/Contents/Resources/Server/lib/framer-package-loader.js!./node_modules/@framer/google.material-design/build/index.js ***!
